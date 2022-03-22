858a2bfda949f938c640009018b14bde71f64062
Leandro Reis <leandro.reiz@gmail.com>
2022-03-22 20:39:37 +0000

    Day 45 - Mobile Navigation Menu

---
+++ Day 45 - Mobile Navigation Menu/script.js
@@ -0,0 +1,17 @@
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const nav = document.querySelectorAll('.nav');
+
+console.log(nav);
+
+openBtn.addEventListener('click', () => {
+  nav.forEach((navEl) => {
+    navEl.classList.add('visible');
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  nav.forEach((navEl) => {
+    navEl.classList.remove('visible');
+  });
+});
